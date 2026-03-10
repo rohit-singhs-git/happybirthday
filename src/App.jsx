c1e1a7ae0b3ab7454e181be61ea83921e0f18481
Rohit Singh <smartyonpc@gmail.com>
2026-03-11 01:01:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Photobook from "./Photobook";
+
+function App() {
+  return (
+    <div className="w-screen h-screen p-2 flex flex-col items-center overflow-hidden">
+      <Photobook/>
+    </div>
+  );
+}
+
+export default App;
